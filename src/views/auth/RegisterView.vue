9b0dcc99d1c986ee730ed113c355c596b7995142
Calvin Kamp <calvin.kamp6696@gmail.com>
2026-06-28 00:43:55 +0200

    feat: implement register-form in register-view

--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
-
+import RegisterForm from '@/components/RegisterForm.vue'
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import CardDescription from '@/components/ui/card/CardDescription.vue'
 </script>
 
 <template>
-  <h1>
-    Auth - RegisterView
-  </h1>
+  <Card>
+    <CardHeader>
+      <CardTitle>
+        Create an Account
+      </CardTitle>
+
+      <CardDescription>
+        Enter your information below to create your account.
+      </CardDescription>
+    </CardHeader>
+
+    <CardContent>
+      <RegisterForm />
+    </CardContent>
+  </Card>
 </template>
